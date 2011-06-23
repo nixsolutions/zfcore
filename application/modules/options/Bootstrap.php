36a7a02404ac3401fe3ad95c5428dee2f4b70725
abyr <abyr@nixsolutions.com>
2011-06-23 11:53:16 +0300

    Options module bootsrtap was filled

---
+++ application/modules/options/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Bootstrap Options Module
+ *
+ * @category   Application
+ * @package    Options
+ * @subpackage Bootstrap
+ * 
+ * @version  $Id$
+ */
+class Options_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+
+}
