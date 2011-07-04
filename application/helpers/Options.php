1b564f1e3a6864e1a3c727cb2252397df2e85262
Maks Slesarenko <maks.slesarenko@gmail.com>
2011-07-04 19:21:54 +0300

    added helper options

---
+++ application/helpers/Options.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Helper_Options
+ *
+ * @version $Id$
+ */
+class Helper_Options extends Zend_Controller_Action_Helper_Abstract
+{
+    /**
+     * Direct
+     *
+     * @param  string $key
+     * @param  string $namespace
+     * @return mixed
+     */
+    public function direct($key, $namespace = 'default')
+    {
+        return $this->get($key, $namespace);
+    }
+
+    /**
+     * Get all options from namespace
+     *
+     * @param string $namespace
+     * @return array
+     */
+    public function getNamespace($namespace)
+    {
+        return Options_Model_Options_Manager::getNamespace($namespace);
+    }
+
+    /**
+     * Set namespace
+     *
+     * @param string $namespace
+     * @param array $options
+     */
+    public function setNamespace($namespace, array $options)
+    {
+        return Options_Model_Options_Manager::getNamespace($namespace, $options);
+    }
+
+    /**
+     * Get option
+     *
+     * @param  string $key
+     * @param  string $namespace
+     * @return mixed
+     */
+    public function get($key, $namespace = 'default')
+    {
+        return Options_Model_Options_Manager::get($key, $namespace);
+    }
+
+    /**
+     * Set option
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     * @param  string $type
+     */
+    public function set($key, $value, $namespace = 'default', $type = null)
+    {
+        return Options_Model_Options_Manager::set($key, $value, $namespace, $type);
+    }
+
+}
