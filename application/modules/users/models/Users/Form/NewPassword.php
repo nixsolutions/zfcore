6dcc3d6d2526e65cae7318b4bba187d82931b13a
naxel <alexander.khaylo@nixsolutions.com>
2011-07-06 18:31:48 +0300

    modefy form

--- application/modules/users/models/Users/Form/NewPassword.php
+++ application/modules/users/models/Users/Form/NewPassword.php
@@ -14,17 +14,18 @@ class Users_Model_Users_Form_NewPassword extends Zend_Form
         $this->setName('userNewPasswordForm');
 
         $passw = new Zend_Form_Element_Password('passw');
-        $passw->setRequired(true)
+        $passw->setLabel('Password')
+            ->setRequired(true)
             ->setValue(null)
             ->addValidator(
                 'StringLength',
                 false,
                 array(Users_Model_User::MIN_PASSWORD_LENGTH)
             );
-        $passw->setDecorators(array('ViewHelper'));
 
         $passwAgain = new Zend_Form_Element_Password('passw_again');
-        $passwAgain->setRequired(true)
+        $passwAgain->setLabel('Password again')
+            ->setRequired(true)
             ->setValue(null)
             ->setValidators(
                 array(
@@ -35,11 +36,9 @@ class Users_Model_Users_Form_NewPassword extends Zend_Form
                     array('Identical', false, array('token' => 'passw'))
                 )
             );
-        $passwAgain->setDecorators(array('ViewHelper'));
 
         $change = new Zend_Form_Element_Submit('change');
         $change->setLabel('Change');
-        $change->setDecorators(array('ViewHelper'));
 
         $this->addElements(array($passw, $passwAgain, $change));
 
