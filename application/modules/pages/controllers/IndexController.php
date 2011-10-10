92074a26b3408ddf40cc7741986b6f302a83f02d
rewolf <rewolf@nixsolutions.com>
2011-10-10 18:24:37 +0300

    pages

--- application/modules/pages/controllers/IndexController.php
+++ application/modules/pages/controllers/IndexController.php
@@ -1,66 +1,79 @@
 <?php
 /**
- * PagesController for default module
+ * PagesController
  *
  * @category   Application
  * @package    Pages
  * @subpackage Controller
- * 
- * @version  $Id: IndexController.php 146 2010-07-05 14:22:20Z AntonShevchuk $
  */
 class Pages_IndexController extends Core_Controller_Action
 {
     /**
      * Init Controller
+     *
+     * @return void
      */
     public function init()
     {
         parent::init();
         $this->_helper->contextSwitch
-                      ->setActionContext('sitemapxml', 'xml')
-                      ->initContext('xml');
+            ->setActionContext('sitemapxml', 'xml')
+            ->initContext('xml');
+
+        $this->_before('_loadPage', array('only' => 'index'));
     }
+
     /**
      * indexAction
      *
-     * Index action in Pages Controller
-     *
-     * @class Static
-     * @access public
-     * @created Wed Aug 06 13:09:14 EEST 2008
+     * @return void
      */
     public function indexAction()
     {
-        $pageTable = new Pages_Model_Page_Table();
-        
-        if (is_null($this->_getParam('alias'))
-            or !($page = $pageTable->getByAlias($this->_getParam('alias')))) {
-            return $this->_forward('notfound', 'error', 'default');
-        }
-        
-        $this->view->Page = $page;
+        $this->view->page = $this->page;
     }
-    
+
     /**
      * Sitemap
      *
+     * @return void
      */
     public function sitemapxmlAction()
     {
         $this->view->layout()->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
         echo $this->view->navigation()->sitemap()
-                                      ->setFormatOutput(true)
-                                      ->setUseSitemapValidators(false);
+            ->setFormatOutput(true)
+            ->setUseSitemapValidators(false);
     }
-    
+
     /**
      * Sitemap
      *
+     * @return void
      */
     public function sitemapAction()
     {
         
     }
-}
 
+    /**
+     * load page
+     *
+     * @return void
+     */
+    protected function _loadPage()
+    {
+        if (!$alias = $this->_getParam('alias')) {
+            $this->_forwardNotFound();
+        }
+
+        $table = new Pages_Model_Page_Table();
+
+        if (!$page = $table->getByAlias($alias)) {
+            $this->_forwardNotFound();
+        }
+
+        $this->page = $page;
+    }
+}
