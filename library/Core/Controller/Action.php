f191cfb28d1462c560854ceea77c973511e873c8
rewolf <rewolf@nixsolutions.com>
2011-10-10 18:19:51 +0300

    add before filter

--- library/Core/Controller/Action.php
+++ library/Core/Controller/Action.php
@@ -4,19 +4,23 @@
  *
  * Controller class for our application
  *
- * @uses     Core_Controller_Action
- *
  * @category Core
  * @package  Core_Controller
- *
- * @version  $Id: Action.php 170 2010-07-26 10:56:18Z AntonShevchuk $
+ * @uses     Core_Controller_Action
  */
 abstract class Core_Controller_Action extends Zend_Controller_Action
 {
     /**
+     * before stack
+     *
+     * @var array
+     */
+    protected $_before = array();
+
+    /**
      * _isDashboard
      *
-     * set requried options for Dashboard controllers
+     * set required options for Dashboard controllers
      *
      * @return  Core_Controller_Action
      */
@@ -29,5 +33,80 @@ abstract class Core_Controller_Action extends Zend_Controller_Action
         $this->view->addHelperPath('Zend/Dojo/View/Helper/', 'Zend_Dojo_View_Helper');
 
         return $this;
+    }
+
+    /**
+     * add function to stack
+     *
+     * @param $function
+     * @param array $options
+     * @return void
+     */
+    protected function _before($function, $options = array())
+    {
+        $this->_before[] = array(
+            'function' => $function,
+            'options' => $options
+        );
+    }
+
+    /**
+     * init before filter
+     *
+     * @return bool
+     */
+    protected function _initBefore()
+    {
+        $action = $this->getRequest()->getActionName();
+
+        foreach ($this->_before as $item) {
+
+            /** check if function is only for current action */
+            if (!empty($item['options']['only'])) {
+                $only = (array) $item['options']['only'];
+                if (!in_array($action, $only)) {
+                    continue;
+                }
+            }
+
+            /** check if function is skipped for current action */
+            if (!empty($item['options']['skip'])) {
+                $skip = (array) $item['options']['skip'];
+                if (in_array($action, $skip)) {
+                    continue;
+                }
+            }
+
+            /** execute */
+            $functions = (array) $item['function'];
+            foreach ($functions as $function) {
+
+                /** next functions won't be executed if current one returns false */
+                if (false === $this->$function()) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    /**
+     * pre-dispatch routines
+     *
+     * @return void
+     */
+    public function preDispatch()
+    {
+        parent::preDispatch();
+        $this->_initBefore();
+    }
+
+    /**
+     * forward to not found page
+     *
+     * @return void
+     */
+    protected function _forwardNotFound()
+    {
+        $this->_forward('notfound', 'error', 'default');
     }
 }
