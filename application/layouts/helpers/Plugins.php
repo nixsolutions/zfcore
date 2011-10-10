281fa1ffa19187cf4ba12cbc1c2863bc5d32ec8b
rewolf <rewolf@nixsolutions.com>
2011-10-10 12:46:58 +0300

    add plugins helper

---
+++ application/layouts/helpers/Plugins.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * add some plugins
+ *
+ * @category Application
+ * @package  Core_View
+ * @subpackage Helper
+ */
+class Application_View_Helper_Plugins extends Zend_View_Helper_Abstract
+{
+    /**
+     * add some plugins
+     *
+     * @return Application_View_Helper_Plugins
+     */
+    public function plugins()
+    {
+        return $this;
+    }
+
+    /**
+     * add grid
+     *
+     * @return Application_View_Helper_Plugins
+     */
+    public function grid()
+    {
+        $this->view->headScript()
+            ->appendFile($this->view->baseUrl('/scripts/jquery/grid/jquery.grid.js'));
+
+        $this->view->headLink()
+            ->appendStylesheet($this->view->baseUrl('/scripts/jquery/grid/grid.css'));
+
+        return $this;
+    }
+
+    /**
+     * add redactor
+     *
+     * @return Application_View_Helper_Plugins
+     */
+    public function redactor()
+    {
+        $this->view->headScript()
+            ->appendFile($this->view->baseUrl('/scripts/jquery/redactor/redactor.js'));
+
+        $this->view->headLink()
+            ->appendStylesheet($this->view->baseUrl('/scripts/jquery/redactor/css/redactor.css'));
+
+        return $this;
+    }
+}
