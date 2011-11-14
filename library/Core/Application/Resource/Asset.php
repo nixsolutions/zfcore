04c6d964db95e5103918566ccbba9c68576c1407
rewolf <rewolf@nixsolutions.com>
2011-11-14 13:25:09 +0200

    add example of configuration

--- library/Core/Application/Resource/Asset.php
+++ library/Core/Application/Resource/Asset.php
@@ -5,6 +5,26 @@
  * @category Core
  * @package  Core_Application
  * @subpackage Resource
+ *
+ * <code>
+ * configuration
+ *
+ * using simple adapter
+ * resources:
+ *   asset:
+ *     dir: PUBLIC_PATH/assets/
+ *     buildDir: PUBLIC_PATH/assets/builds/
+ *     adapter: Core_Asset_Adapter_Simple
+ *
+ * using yui compressor adapter
+ * resources:
+ *   asset:
+ *     dir: PUBLIC_PATH/assets/
+ *     buildDir: PUBLIC_PATH/assets/builds/
+ *     adapter:
+ *       class: Core_Asset_Adapter_YuiCompressor
+ *       jarPath: APPLICATION_PATH/../data/yuicompressor-2.4.6.jar
+ * </code>
  */
 class Core_Application_Resource_Asset extends Zend_Application_Resource_ResourceAbstract
 {
