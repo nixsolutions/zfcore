f1bf762f2d22cc3fe033df129137213aeb80fc6c
Alexander Khaylo <alex.khaylo@gmail.com>
2012-01-11 16:19:40 +0200

    fix Model_UserTest

--- tests/application/modules/users/models/UserTest.php
+++ tests/application/modules/users/models/UserTest.php
@@ -15,30 +15,29 @@ class Model_UserTest extends ControllerTestCase
     public function setUp()
     {
         parent::setUp();
-        
+
         $this->_fixture = array('login'    => 'testguest'.uniqid(),
                                 'email'    => 'testguest@domain.com',
-                                'ip'       => '10.10.10.10',
                                 'role'     => Users_Model_User::ROLE_GUEST,
                                 'status'   => Users_Model_User::STATUS_ACTIVE,
                                 'password' => 123456);
-        
+
         $this->_userTable = new Users_Model_Users_Table();
-        
-        $this->_user = $this->_userTable->create($this->_fixture);
+
+        $this->_user = $this->_userTable->createRow($this->_fixture);
         $this->_user->save();
     }
-    
+
     /**
      * Test get password
      *
      */
     public function testGetPassword()
     {
-        $this->assertNull($this->_user->password);
+        $this->assertEquals($this->_user->password, md5($this->_user->salt . $this->_fixture['password']));
     }
-    
-    
+
+
     /**
      * Test get password
      *
@@ -48,12 +47,12 @@ class Model_UserTest extends ControllerTestCase
         $password = 654321;
         $this->_user->password = $password;
         $this->_user->save();
-        
+
         $this->assertTrue(
             Users_Model_Users_Manager::authenticate($this->_user->login, $password)
         );
     }
-    
+
     /**
      * Test get ip
      *
@@ -63,7 +62,7 @@ class Model_UserTest extends ControllerTestCase
         $result = preg_match('/(\d+\.\d+\.\d+\.\d+)/u', $this->_user->ip);
         $this->assertGreaterThan(0, $result);
     }
-    
+
     /**
      * Test set ip
      *
@@ -73,10 +72,10 @@ class Model_UserTest extends ControllerTestCase
         $ip = '11.11.11.11';
         $this->_user->ip = $ip;
         $this->_user->save();
-        
+
         $this->assertEquals($ip, $this->_user->ip);
     }
-    
+
     /**
      * test toArray
      *
@@ -86,17 +85,15 @@ class Model_UserTest extends ControllerTestCase
         //safe method
         $user = $this->_user->toArray(true);
         unset($this->_fixture['password']);
-        
+
         $this->assertEquals(array_merge($user, $this->_fixture), $user);
-        
-        $this->assertNull($user['password']);
-        
+
         //default method
         $user = $this->_user->toArray();
         $this->assertGreaterThanOrEqual(32, strlen($user['password']));
-        
+
     }
-    
+
     /**
      * Remove environtment
      */
@@ -104,7 +101,7 @@ class Model_UserTest extends ControllerTestCase
     {
         $user = $this->_userTable->getByLogin($this->_fixture['login']);
         $user->delete();
-        
+
         parent::tearDown();
     }
 }
