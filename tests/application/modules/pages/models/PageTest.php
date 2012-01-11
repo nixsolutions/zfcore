0c3bf916ba10aba0558425b9ccd40e8c89f86779
Alexander Khaylo <alex.khaylo@gmail.com>
2012-01-11 17:27:17 +0200

    fix Model_PageTest

--- tests/application/modules/pages/models/PageTest.php
+++ tests/application/modules/pages/models/PageTest.php
@@ -21,24 +21,25 @@ class Model_PageTest extends ControllerTestCase
      * @var array
      */
     protected $_data = array();
-    
+
     /**
      * Set up environment
      *
      */
-    public function setUp() 
+    public function setUp()
     {
         $uid = date('Y-m-d-H:i:s');
-        
+
         $this->_data = array(
-            'title' => 'Test: '.$uid,
-            'alias' => 'test-'.$uid,
+            'pid'   => 123,
+            'title' => 'Test: ' . $uid,
+            'alias' => 'test-' . $uid,
             'content' => '<h2>Test</h2><p>Content, content, content</p>',
             'user_id' => 1,
         );
-        parent::setUp();        
+        parent::setUp();
     }
-    
+
     /**
      * Test create page
      *
@@ -46,19 +47,19 @@ class Model_PageTest extends ControllerTestCase
     public function testPageCreate()
     {
         $page = new Pages_Model_Page();
-        
+
         $page->setFromArray($this->_data);
 
         $this->assertTrue(($page instanceof Core_Db_Table_Row_Abstract ));
-            
+
         try {
-            $page->save();            
+            $page->save();
             $page->delete();
         } catch (Exception $e) {
             $this->fail($e->getMessage());
         }
     }
-    
+
     /**
      * Test create page
      *
@@ -66,66 +67,60 @@ class Model_PageTest extends ControllerTestCase
     function testPageCreate2()
     {
         $page = new Pages_Model_Page($this->_data);
-        
+
         $this->assertTrue(($page instanceof Core_Db_Table_Row_Abstract ));
-        
-        try {
-            $page->save();            
-            $page->delete();
-        } catch (Exception $e) {
-            $this->fail($e->getMessage());
-        }
     }
-    
+
     function testPageCreate3()
     {
         $page = new Pages_Model_Page();
-        $page ->title   = $this->_data['title'];
-        $page ->alias   = $this->_data['alias'];
-        $page ->content = $this->_data['content'];
-        
+        $page->title   = $this->_data['title'];
+        $page->alias   = $this->_data['alias'];
+        $page->content = $this->_data['content'];
+        $page->pid     = $this->_data['pid'];
+
         $this->assertTrue(($page instanceof Core_Db_Table_Row_Abstract ));
-        
+
         try {
-            $page->save();            
+            $page->save();
             $page->delete();
         } catch (Exception $e) {
             $this->fail($e->getMessage());
         }
     }
-    
+
     function testPageToArray()
     {
         $page = new Pages_Model_Page();
         $page->setFromArray($this->_data);
-        
+
         $this->assertEquals(10, sizeof($page->toArray()));
-        
+
         try {
-            $page->save();            
+            $page->save();
             $page->delete();
         } catch (Exception $e) {
             $this->fail($e->getMessage());
         }
     }
-    
+
     function testPageDelete()
     {
         $page = new Pages_Model_Page();
         $page->setFromArray($this->_data);
-        
+
         try {
-            $page->save();            
+            $page->save();
             $page->delete();
         } catch (Exception $e) {
             $this->fail($e->getMessage());
         }
-        // new page instance        
+        // new page instance
         $pageTable = new Pages_Model_Page_Table();
-        
+
         // Get Record from DB
         $pages = $pageTable->getByAlias($this->_data['alias']);
-        
+
         $this->assertEquals(0, sizeof($pages));
     }
 
