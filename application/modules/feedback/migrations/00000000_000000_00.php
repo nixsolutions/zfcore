2aaa49ae652e035142bd4af5ab845b77f7546a8c
Alexander Khaylo <alex.khaylo@gmail.com>
2012-01-23 19:23:34 +0200

    fix feedback migration

--- application/modules/feedback/migrations/00000000_000000_00.php
+++ application/modules/feedback/migrations/00000000_000000_00.php
@@ -6,47 +6,22 @@ class Feedback_Migration_00000000_000000_00 extends Core_Migration_Abstract
     public function up()
     {
         // feedback table
-        $this->createTable('feedback');
-        
-        $this->createColumn('feedback',
-                            'sender', 
-                            Core_Migration_Abstract::TYPE_TEXT, 
-                            null, null, true);
-                            
-        $this->createColumn('feedback',
-                            'subject', 
-                            Core_Migration_Abstract::TYPE_TEXT, 
-                            null, null, true);
-                            
-        $this->createColumn('feedback',
-                            'message', 
-                            Core_Migration_Abstract::TYPE_TEXT, 
-                            null, null, true);
-                            
-        $this->createColumn('feedback',
-                            'email', 
-                            Core_Migration_Abstract::TYPE_VARCHAR, 
-                            80, null, true);
-          
-        $this->createColumn('feedback',
-                            'status', 
-                            Core_Migration_Abstract::TYPE_ENUM,
-                            array('New', 'Read', 'Reply', 'Edit', 'Delete'), 'New', true);
-                            
-        $this->createColumn('feedback',
-                            'created', 
-                            Core_Migration_Abstract::TYPE_TIMESTAMP, 
-                            null, 'CURRENT_TIMESTAMP');
-       
-        $this->createColumn('feedback',
-                            'updated', 
-                            Core_Migration_Abstract::TYPE_TIMESTAMP, 
-                            null, '2000-01-01 00:00');
-                            
-        $this->createColumn('feedback',
-                            'data', 
-                            Core_Migration_Abstract::TYPE_TEXT,
-                            null, null);
+
+        $this->query(
+            "
+                    CREATE TABLE `feedback` (
+                      `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+                      `sender` TEXT NOT NULL,
+                      `subject` TEXT NOT NULL,
+                      `message` TEXT NOT NULL,
+                      `email` VARCHAR(320) NOT NULL,
+                      `status` ENUM('New','Read','Reply','Edit','Delete') NOT NULL DEFAULT 'New',
+                      `created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+                      `updated` TIMESTAMP NULL DEFAULT '2000-01-01 00:00:00',
+                      `data` TEXT,
+                      PRIMARY KEY  (`id`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8"
+        );
     }
 
     public function down()
