f03729c963f7d9344a1c50d1fd6e1dc4146cce90
Alexander Khaylo <alex.khaylo@gmail.com>
2012-01-23 19:30:42 +0200

    faq/migrations

--- application/modules/faq/migrations/00000000_000000_00.php
+++ application/modules/faq/migrations/00000000_000000_00.php
@@ -8,15 +8,19 @@ class Faq_Migration_00000000_000000_00 extends Core_Migration_Abstract
         // faq table
         $this->createTable('faq');
         
-        $this->createColumn('faq',
-                            'question', 
-                            Core_Migration_Abstract::TYPE_TEXT, 
-                            null, null, true);
+        $this->createColumn(
+            'faq',
+            'question',
+            Core_Migration_Abstract::TYPE_TEXT,
+            null, null, true
+        );
                             
-        $this->createColumn('faq',
-                            'answer', 
-                            Core_Migration_Abstract::TYPE_TEXT, 
-                            null, null, true);
+        $this->createColumn(
+            'faq',
+            'answer',
+            Core_Migration_Abstract::TYPE_TEXT,
+            null, null, true
+        );
     }
 
     public function down()
