9ee5f86d7a0ea85cecbdef54e6ba9a902dc8c659
Alexander Khaylo <alex.khaylo@gmail.com>
2012-01-24 18:33:41 +0200

    Formatting Core_Tool_Project_Context_Core_MigrationFile

--- library/Core/Tool/Project/Context/Core/MigrationFile.php
+++ library/Core/Tool/Project/Context/Core/MigrationFile.php
@@ -58,8 +58,8 @@ class Core_Tool_Project_Context_Core_MigrationFile
     public function init()
     {
         if (null === $this->_migrationName) {
-            list($sec, $msec) = explode( ".", microtime( true ) );
-            $this->_migrationName = date( 'Ymd_His_' ) . sprintf( "%02d", $msec );
+            list($sec, $msec) = explode(".", microtime(true));
+            $this->_migrationName = date('Ymd_His_') . sprintf("%02d", $msec);
         }
 
         $this->_filesystemName = $this->_migrationName . '.php';
@@ -71,24 +71,24 @@ class Core_Tool_Project_Context_Core_MigrationFile
     {
         // Configuring after instantiation
         $methodUp = new Zend_CodeGenerator_Php_Method();
-        $methodUp->setName( 'up' )
-            ->setBody( '// upgrade' );
+        $methodUp->setName('up')
+            ->setBody('// upgrade');
 
         // Configuring after instantiation
         $methodDown = new Zend_CodeGenerator_Php_Method();
-        $methodDown->setName( 'down' )
-            ->setBody( '// degrade' );
+        $methodDown->setName('down')
+            ->setBody('// degrade');
 
 
         $class = new Zend_CodeGenerator_Php_Class();
-        $class->setName( 'Migration_' . $this->getMigrationName() )
-            ->setExtendedClass( 'Core_Migration_Abstract' )
-            ->setMethod( $methodUp )
-            ->setMethod( $methodDown );
+        $class->setName('Migration_' . $this->getMigrationName())
+            ->setExtendedClass('Core_Migration_Abstract')
+            ->setMethod($methodUp)
+            ->setMethod($methodDown);
 
         $file = new Zend_CodeGenerator_Php_File();
-        $file->setClass( $class )
-            ->setFilename( $this->getPath() );
+        $file->setClass($class)
+            ->setFilename($this->getPath());
 
         return $file->generate();
     }
