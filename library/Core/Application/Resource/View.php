0b0dc44630b7dbfdbead15df19f387f56ba1c3c9
Alexander Khaylo <alex.khaylo@gmail.com>
2012-01-24 17:24:28 +0200

    Formatting Core_Application_Resource_View

--- library/Core/Application/Resource/View.php
+++ library/Core/Application/Resource/View.php
@@ -42,20 +42,20 @@ class Core_Application_Resource_View extends Zend_Application_Resource_View
             $this->_view = new Core_View($options);
 
             if (isset($options['title'])) {
-                $this->_view->headTitle( $options['title'] );
+                $this->_view->headTitle($options['title']);
             }
 
             if (isset($options['doctype'])) {
-                $this->_view->doctype()->setDoctype( strtoupper( $options['doctype'] ) );
+                $this->_view->doctype()->setDoctype(strtoupper($options['doctype']));
                 if (isset($options['charset']) && $this->_view->doctype()->isHtml5()) {
-                    $this->_view->headMeta()->setCharset( $options['charset'] );
+                    $this->_view->headMeta()->setCharset($options['charset']);
                 }
             }
             if (isset($options['contentType'])) {
-                $this->_view->headMeta()->appendHttpEquiv( 'Content-Type', $options['contentType'] );
+                $this->_view->headMeta()->appendHttpEquiv('Content-Type', $options['contentType']);
             }
-            if (isset($options['assign']) && is_array( $options['assign'] )) {
-                $this->_view->assign( $options['assign'] );
+            if (isset($options['assign']) && is_array($options['assign'])) {
+                $this->_view->assign($options['assign']);
             }
         }
         return $this->_view;
