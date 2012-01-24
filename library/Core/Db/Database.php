458c312387af70856cc7ec9065ee56cb4c453be3
malinovskiy <malinovskiy@glide.nixsolutions.com>
2012-01-24 16:22:59 +0200

    move query-build methods to Core_Db_Database

---
+++ library/Core/Db/Database.php
@@ -0,0 +1,445 @@
+<?php
+/**
+ * Created by glide/malinovskiy.
+ * Date: 24.01.12
+ * Time: 15:46
+ */
+class Core_Db_Database
+{
+    /**
+     * array with schemes of tables
+     * @var array
+     */
+    protected $_scheme = array();
+
+    /**
+     * array with indexes of tables
+     * @var array
+     */
+    protected $_indexes = array();
+
+    /**
+     * array with ignored tables
+     * @var array
+     */
+    protected $_blackList = array();
+
+    /**
+     * array with "white listed" tables
+     * @var array
+     */
+    protected $_whiteList = array();
+
+    /**
+     * @param null $options
+     * @param bool $autoLoad
+     */
+    public function __construct($options = null,$autoLoad = true)
+    {
+        if (isset($options['blacklist']) && !isset($options['whitelist'])) {
+
+            if(is_array($options['blacklist'])) {
+                $this->_blackList = $options['blacklist'];
+            } else {
+                $this->_blackList[] = (string)$options['blacklist'];
+            }
+
+        } elseif (isset($options['whitelist']) && !empty($options['whitelist'])) {
+
+            if(is_array($options['whitelist'])) {
+                $this->_whiteList = $options['whitelist'];
+            } else {
+                $this->_whiteList[] = (string)$options['whitelist'];
+            }
+
+        }
+
+
+        if($autoLoad) {
+            $dbAdapter = Zend_Db_Table::getDefaultAdapter();
+            $tables  = $dbAdapter->listTables();
+
+            foreach($tables as $table) {
+
+                $scheme = $dbAdapter->describeTable($table);
+
+                $this->addTable($table,$scheme);
+
+                $this->_indexes[$table] = $this->getIndexListFromTable($table);
+
+            }
+        }
+
+    }
+
+    /**
+     * retrieve index list from table
+     * @param $table - table name
+     * @return array - array of indexes
+     */
+
+    protected function getIndexListFromTable($table)
+    {
+        $db = Zend_Db_Table::getDefaultAdapter();
+
+        $sql = "SHOW INDEXES FROM `{$table}`";
+        $indexesData = $db->fetchAll($sql);
+        $indexes = array();
+
+        foreach($indexesData as $index) {
+            if (!isset($indexes[$index['Key_name']])) $indexes[$index['Key_name']] = array();
+            $indexes[$index['Key_name']]['unique'] = !intval($index['Non_unique']);
+            $indexes[$index['Key_name']]['type'] = $index['Index_type'];
+            $indexes[$index['Key_name']]['name'] = $index['Key_name'];
+            $indexes[$index['Key_name']]['table'] = $index['Table'];
+            if (!isset($indexes[$index['Key_name']]['fields'])) $indexes[$index['Key_name']]['fields'] = array();
+            $indexes[$index['Key_name']]['fields'][$index['Seq_in_index']] =
+                array(
+                    'name' => $index['Column_name'],
+                    'length' => $index['Sub_part']
+                );
+            $indexes[$index['Key_name']]['constraint'] = $this->getConstraintForColumn($table, $index['Column_name']);
+
+        }
+        return $indexes;
+
+    }
+
+    /**
+     * @param $table - table name
+     * @param $colName - column name
+     * @return array|bool - return list of constrains or false, if constrains not exist
+     */
+
+    protected function getConstraintForColumn($table, $colName)
+    {
+        $db = Zend_Db_Table::getDefaultAdapter();
+
+        $row = $db->fetchRow("select database() as dbname");
+
+        $dbName = $row['dbname'];
+
+        $sql = "SELECT k.CONSTRAINT_SCHEMA,k.CONSTRAINT_NAME,"
+            ."k.TABLE_NAME,k.COLUMN_NAME,k.REFERENCED_TABLE_NAME,"
+            ."k.REFERENCED_COLUMN_NAME, r.UPDATE_RULE, r.DELETE_RULE FROM "
+            ."information_schema.key_column_usage k LEFT JOIN "
+            ."information_schema.referential_constraints r ON "
+            ."r.CONSTRAINT_SCHEMA = k.CONSTRAINT_SCHEMA AND "
+            ." k.REFERENCED_TABLE_NAME=r.REFERENCED_TABLE_NAME "
+            ."LEFT JOIN information_schema.table_constraints t ON "
+            ."t.CONSTRAINT_SCHEMA = r.CONSTRAINT_SCHEMA WHERE "
+            ." k.constraint_schema='$dbName' AND t.CONSTRAINT_TYPE='FOREIGN KEY' "
+            ."AND k.TABLE_NAME='$table' AND r.TABLE_NAME='$table' "
+            ." AND t.TABLE_NAME='$table' AND k.COLUMN_NAME='$colName'";
+
+        $row = $db->fetchRow($sql);
+
+        if (!count($row)) return false;
+
+        $constraint = array(
+            'table' => $table,
+            'name' => $row['CONSTRAINT_NAME'],
+            'column' => $row['COLUMN_NAME'],
+            'reference' => array(
+                'table' => $row['REFERENCED_TABLE_NAME'],
+                'column' => $row['REFERENCED_COLUMN_NAME'],
+                'update' => $row['UPDATE_RULE'],
+                'delete' => $row['DELETE_RULE'],
+            )
+        );
+        return $constraint;
+    }
+
+    /**
+     * add table to DB object
+     * @param $tableName
+     * @param $scheme - table structure from DESCRIBE TABLE query
+     */
+
+    public function addTable($tableName, $scheme)
+    {
+        if($this->isTblWhiteListed($tableName) && !$this->isTblBlackListed($tableName))
+            $this->_scheme[$tableName] = $scheme;
+    }
+
+    /**
+     * delete table from DB object
+     * @param $tableName
+     */
+    public function deleteTable($tableName)
+    {
+        if(array_key_exists($tableName,$this->_scheme))
+            unset($this->_scheme[$tableName]);
+    }
+    /**
+     * checks for table in @blacklist
+     * @param $tableName
+     * @return bool
+     */
+    protected function isTblWhiteListed($tableName)
+    {
+        if(!empty($this->_whiteList)) {
+            return in_array($tableName,$this->_whiteList);
+        }
+        return true;
+    }
+
+    /**
+     * checks for table in @whitelist
+     * @param $tableName
+     * @return bool
+     */
+    protected function isTblBlackListed($tableName)
+    {
+        if(!empty($this->_blackList)) {
+            return in_array($tableName,$this->_blackList);
+        }
+        return false;
+
+    }
+
+    /**
+     * encode object data into JSON
+     * @return string - JSON string
+     */
+    public function toString()
+    {
+        return json_encode(
+            array('data'=>$this->_scheme,'indexes'=>$this->_indexes)
+        );
+    }
+
+    /**
+     * decode object from JSON string
+     * clear scheme and indexes if string is empty
+     * @param $jsonString
+     */
+    public function fromString($jsonString)
+    {
+        if (!empty($jsonString)) {
+
+            $dec = json_decode($jsonString,true);
+
+            $this->_indexes = $dec['indexes'];
+            $dec = $dec['data'];
+
+            foreach($this->_blackList as $deleteKey){
+                if(array_key_exists($deleteKey,$dec)) {
+                    unset($dec[$deleteKey]);
+                }
+            }
+            foreach($dec as $tblName=>$table){
+                if(!in_array($tblName,$this->_whiteList)) {
+                    unset($dec[$tblName]);
+                }
+            }
+
+            $this->_scheme = $dec;
+        } else {
+            $this->_scheme = array();
+            $this->_indexes = array();
+        }
+
+    }
+
+    /**
+     * get all tables form DB
+     * @return array
+     */
+    public function getTables()
+    {
+        return $this->_scheme;
+    }
+
+    /**
+     * get all columns from table
+     * @param $tableName
+     * @return array|bool - returns false if table not exist
+     */
+    public function getTableColumns($tableName)
+    {
+        return (isset($this->_scheme[$tableName])) ?
+            $this->_scheme[$tableName] : false;
+
+    }
+
+    /**
+     * get all table indexes
+     * @param $tableName
+     * @return array - returns empty array if no indexes found
+     */
+    public function getIndexList($tableName)
+    {
+        if(array_key_exists($tableName,$this->_indexes))
+            return $this->_indexes[$tableName];
+        else
+            return array();
+    }
+
+
+
+    /**
+     * create DROP TABLE query
+     * @param $tableName
+     * @return string
+     */
+    public static function dropTable($tableName)
+    {
+        return "DROP TABLE IF EXISTS `{$tableName}`";
+    }
+
+    /**
+     * create query for delete column
+     * @param $tableName
+     * @param $column
+     * @return string
+     */
+    public static function dropColumn($tableName, $column)
+    {
+        return "ALTER TABLE `{$tableName}` DROP `{$column['COLUMN_NAME']}`";
+    }
+
+    /**
+     * add column attributes to query
+     * @param $sql
+     * @param $column
+     */
+    protected static function addSqlExtras(& $sql, $column)
+    {
+        if ($column['LENGTH']) $sql .= ' (' . $column['LENGTH'] . ')';
+        if ($column['UNSIGNED']) $sql .= ' UNSIGNED ';
+
+        if (!$column['NULLABLE']) $sql .= " NOT NULL ";
+        if (!is_null($column['DEFAULT'])) $sql .= " DEFAULT \\'{$column['DEFAULT']}\\' ";
+        if ($column['IDENTITY']) $sql .= ' AUTO_INCREMENT ';
+
+    }
+
+    /**
+     * create query for adding column
+     * @param $tableName
+     * @param $column
+     * @return string
+     */
+    public static function addColumn($tableName, $column)
+    {
+        $sql = "ALTER TABLE `{$tableName}` ADD `{$column['COLUMN_NAME']}` " . addslashes($column['DATA_TYPE']);
+        Core_Db_Database::addSqlExtras($sql, $column);
+        return $sql;
+    }
+
+    /**
+     * create query for change column
+     * @param $tableName
+     * @param $column
+     * @return string
+     */
+    public static function changeColumn($tableName, $column)
+    {
+        $sql = "ALTER TABLE `{$tableName}` CHANGE " .
+            " `{$column['COLUMN_NAME']}` `{$column['COLUMN_NAME']}` " .
+            addslashes($column['DATA_TYPE']);
+        Core_Db_Database::addSqlExtras($sql, $column);
+        return $sql;
+    }
+    /**
+     * create CREATE TABLE query
+     * @param $tblName
+     * @return string
+     */
+    public static function createTable($tblName)
+    {
+        $db = Zend_Db_Table::getDefaultAdapter();
+
+        $trow = $db->fetchRow("SHOW CREATE TABLE `{$tblName}`");
+        $query = preg_replace('#AUTO_INCREMENT=\S+#is', '', $trow['Create Table']);
+        //$query = preg_replace("#\n\s*#", ' ', $query); //uncomment if you want query in one line
+        $query = addcslashes($query, '\\\''); //escape slashes and single quotes
+        return $query;
+    }
+
+    /**
+     * create query for adding index
+     * @param $index
+     * @return string
+     */
+    public static function addIndex($index)
+    {
+        if ($index['name'] === 'PRIMARY') {
+            $index_string = "ALTER TABLE `{$index['table']}` ADD PRIMARY KEY";
+            $fields = array();
+            foreach ($index['fields'] as $f)
+            {
+                $len = intval($f['length']) ? "({$f['length']})" : '';
+                $fields[] = "{$f['name']}" . $len;
+            }
+            $index_string .= "(" . implode(',', $fields) . ")";
+        } else {
+            $index_string = "CREATE ";
+            if ($index['type'] === 'FULLTEXT') $index_string .= " FULLTEXT ";
+            if ($index['unique']) $index_string .= " UNIQUE ";
+            $index_string .= " INDEX `{$index['name']}` ";
+            if (in_array($index['type'], array('RTREE', 'BTREE', 'HASH',))) {
+                $index_string .= " USING {$index['type']} ";
+            }
+            $index_string .= " on `{$index['table']}` ";
+            $fields = array();
+            foreach ($index['fields'] as $f)
+            {
+                $len = intval($f['length']) ? "({$f['length']})" : '';
+                $fields[] = "{$f['name']}" . $len;
+            }
+            $index_string .= "(" . implode(',', $fields) . ")";
+        }
+        return $index_string;
+    }
+
+    /**
+     * create query for drop index
+     * @param $index
+     * @return string
+     */
+    public static function dropIndex($index)
+    {
+        return "DROP INDEX `{$index['name']}` ON `{$index['table']}`";
+    }
+
+    /**
+     * create query for drop constraint
+     * @param $index
+     * @return string
+     */
+    public static function dropConstraint($index)
+    {
+        if (!isset($index['constraint']['column'])
+            || !strlen($index['constraint']['column'])
+        ) {
+            return '';
+        }
+
+        $sql = "ALTER TABLE `{$index['constraint']['table']}` " .
+            "DROP FOREIGN KEY `{$index['constraint']['name']}` ";
+
+        return $sql;
+    }
+
+    /**
+     * create query for adding constraint
+     * @param $index
+     * @return string
+     */
+    public static function addConstraint($index)
+    {
+        if (!isset($index['constraint']['column']) || !strlen($index['constraint']['column'])) return '';
+        $sql = "ALTER TABLE `{$index['constraint']['table']}` " .
+            "ADD CONSTRAINT `{$index['constraint']['name']}` " .
+            "FOREIGN KEY (`{$index['constraint']['column']}`) " .
+            "REFERENCES `{$index['constraint']['reference']['table']}` " .
+            "(`{$index['constraint']['reference']['column']}`) " .
+            "ON UPDATE {$index['constraint']['reference']['update']} " .
+            "ON DELETE {$index['constraint']['reference']['delete']} ";
+        return $sql;
+    }
+
+}
+
--- library/Core/Db/Database/Diff.php
+++ library/Core/Db/Database/Diff.php
@@ -102,9 +102,12 @@ class Core_Db_Database_Diff
      */
     protected function addCreateTable($tableName)
     {
-        $this->down($this->dropTable($tableName));
-        $this->up($this->dropTable($tableName));
-        $this->up($this->createTable($tableName));
+        $this->down(Core_Db_Database::dropTable($tableName));
+        
+        //Core_Db_Database::dropTable()
+        
+        $this->up(Core_Db_Database::dropTable($tableName));
+        $this->up(Core_Db_Database::createTable($tableName));
     }
     /**
      * add drop creation action to "upgrade"
@@ -112,9 +115,9 @@ class Core_Db_Database_Diff
      */
     protected function addDropTable($tableName)
     {
-        $this->up($this->dropTable($tableName));
-        $this->down($this->dropTable($tableName));
-        $this->down($this->createTable($tableName));
+        $this->up(Core_Db_Database::dropTable($tableName));
+        $this->down(Core_Db_Database::dropTable($tableName));
+        $this->down(Core_Db_Database::createTable($tableName));
     }
 
     /**
@@ -149,15 +152,15 @@ class Core_Db_Database_Diff
             $colForCompare = $this->checkColumnExists($currCol, $tblPublishedCols);
 
             if (!$colForCompare) {
-                $this->up($this->addColumn($table, $currCol));
-                $this->down($this->dropColumn($table, $currCol));
+                $this->up(Core_Db_Database::addColumn($table, $currCol));
+                $this->down(Core_Db_Database::dropColumn($table, $currCol));
             }
             else
             {
                 if ($currCol === $colForCompare) continue;
-                $sql = $this->changeColumn($table, $currCol);
+                $sql = Core_Db_Database::changeColumn($table, $currCol);
                 $this->up($sql);
-                $sql = $this->changeColumn($table, $colForCompare);
+                $sql = Core_Db_Database::changeColumn($table, $colForCompare);
                 $this->down($sql);
             }
         }
@@ -169,14 +172,14 @@ class Core_Db_Database_Diff
             $has = $this->checkColumnExists($published_column, $tblCurrentCols);
 
             if (!$has) {
-                $constraint = $this->getConstraintForColumn($this->_publishedDb, $table, $published_column['Field']);
+                $constraint = $this->getConstraintForColumn($table, $published_column['COLUMN_NAME']);
 
                 if (count($constraint)) {
-                    $this->down($this->addConstraint(array('constraint' => $constraint)));
-                    $this->up($this->dropConstraint(array('constraint' => $constraint)));
+                    $this->down(Core_Db_Database::addConstraint(array('constraint' => $constraint)));
+                    $this->up(Core_Db_Database::dropConstraint(array('constraint' => $constraint)));
                 }
-                $this->down($this->addColumn($table, $published_column));
-                $this->up($this->dropColumn($table, $published_column));
+                $this->down(Core_Db_Database::addColumn($table, $published_column));
+                $this->up(Core_Db_Database::dropColumn($table, $published_column));
             }
         }
     }
@@ -195,12 +198,12 @@ class Core_Db_Database_Diff
         {
             $index_for_compare = $this->checkIndexExists($cur_index, $published_indexes);
             if (!$index_for_compare) {
-                $this->down($this->dropConstraint($cur_index));
-                $this->down($this->dropIndex($cur_index));
-                $this->up($this->dropConstraint($cur_index));
-                $this->up($this->dropIndex($cur_index));
-                $this->up($this->addIndex($cur_index));
-                $this->up($this->addConstraint($cur_index));
+                $this->down(Core_Db_Database::dropConstraint($cur_index));
+                $this->down(Core_Db_Database::dropIndex($cur_index));
+                $this->up(Core_Db_Database::dropConstraint($cur_index));
+                $this->up(Core_Db_Database::dropIndex($cur_index));
+                $this->up(Core_Db_Database::addIndex($cur_index));
+                $this->up(Core_Db_Database::addConstraint($cur_index));
             }
             elseif ($index_for_compare === $cur_index)
             {
@@ -208,14 +211,14 @@ class Core_Db_Database_Diff
             }
             else // index exists but not identical
             {
-                $this->down($this->dropConstraint($cur_index));
-                $this->down($this->dropIndex($cur_index));
-                $this->down($this->addIndex($index_for_compare));
-                $this->down($this->addConstraint($index_for_compare));
-                $this->up($this->dropConstraint($cur_index));
-                $this->up($this->dropIndex($cur_index));
-                $this->up($this->addIndex($cur_index));
-                $this->up($this->addConstraint($cur_index));
+                $this->down(Core_Db_Database::dropConstraint($cur_index));
+                $this->down(Core_Db_Database::dropIndex($cur_index));
+                $this->down(Core_Db_Database::addIndex($index_for_compare));
+                $this->down(Core_Db_Database::addConstraint($index_for_compare));
+                $this->up(Core_Db_Database::dropConstraint($cur_index));
+                $this->up(Core_Db_Database::dropIndex($cur_index));
+                $this->up(Core_Db_Database::addIndex($cur_index));
+                $this->up(Core_Db_Database::addConstraint($cur_index));
             }
         }
     }
@@ -231,85 +234,6 @@ class Core_Db_Database_Diff
         return (array_key_exists($column['COLUMN_NAME'], $colList)) ?
             $colList[$column['COLUMN_NAME']] : false;
 
-    }
-
-    /**
-     * create DROP TABLE query
-     * @param $tableName
-     * @return string
-     */
-    protected function dropTable($tableName)
-    {
-        return "DROP TABLE IF EXISTS `{$tableName}`";
-    }
-
-    /**
-     * create query for delete column
-     * @param $tableName
-     * @param $column
-     * @return string
-     */
-    protected function dropColumn($tableName, $column)
-    {
-        return "ALTER TABLE `{$tableName}` DROP `{$column['COLUMN_NAME']}`";
-    }
-
-    /**
-     * create query for adding column
-     * @param $tableName
-     * @param $column
-     * @return string
-     */
-    protected function addColumn($tableName, $column)
-    {
-        $sql = "ALTER TABLE `{$tableName}` ADD `{$column['COLUMN_NAME']}` " . addslashes($column['DATA_TYPE']);
-        $this->addSqlExtras($sql, $column);
-        return $sql;
-    }
-    /**
-     * add column attributes to query
-     * @param $sql
-     * @param $column
-     */
-    protected function addSqlExtras(& $sql, $column)
-    {
-        if ($column['LENGTH']) $sql .= ' (' . $column['LENGTH'] . ')';
-        if ($column['UNSIGNED']) $sql .= ' UNSIGNED ';
-
-        if (!$column['NULLABLE']) $sql .= " NOT NULL ";
-        if (!is_null($column['DEFAULT'])) $sql .= " DEFAULT \\'{$column['DEFAULT']}\\' ";
-        if ($column['IDENTITY']) $sql .= ' AUTO_INCREMENT ';
-
-    }
-    /**
-     * create query for change column
-     * @param $tableName
-     * @param $column
-     * @return string
-     */
-    protected function changeColumn($tableName, $column)
-    {
-        $sql = "ALTER TABLE `{$tableName}` CHANGE " .
-            " `{$column['COLUMN_NAME']}` `{$column['COLUMN_NAME']}` " .
-            addslashes($column['DATA_TYPE']);
-        $this->addSqlExtras($sql, $column);
-        return $sql;
-    }
-
-    /**
-     * create CREATE TABLE query
-     * @param $tblName
-     * @return string
-     */
-    protected function createTable($tblName)
-    {
-        $db = Zend_Db_Table::getDefaultAdapter();
-
-        $trow = $db->fetchRow("SHOW CREATE TABLE `{$tblName}`");
-        $query = preg_replace('#AUTO_INCREMENT=\S+#is', '', $trow['Create Table']);
-        //$query = preg_replace("#\n\s*#", ' ', $query); //uncomment if you want query in one line
-        $query = addcslashes($query, '\\\''); //escape slashes and single quotes
-        return $query;
     }
 
     /**
@@ -329,56 +253,6 @@ class Core_Db_Database_Diff
         return false;
     }
 
-    /**
-     * create query for adding index
-     * @param $index
-     * @return string
-     */
-    protected function addIndex($index)
-    {
-        if ($index['name'] === 'PRIMARY') {
-            $index_string = "ALTER TABLE `{$index['table']}` ADD PRIMARY KEY";
-            $fields = array();
-            foreach ($index['fields'] as $f)
-            {
-                $len = intval($f['length']) ? "({$f['length']})" : '';
-                $fields[] = "{$f['name']}" . $len;
-            }
-            $index_string .= "(" . implode(',', $fields) . ")";
-        } else {
-            $index_string = "CREATE ";
-            if ($index['type'] === 'FULLTEXT') $index_string .= " FULLTEXT ";
-            if ($index['unique']) $index_string .= " UNIQUE ";
-            $index_string .= " INDEX `{$index['name']}` ";
-            if (in_array($index['type'], array('RTREE', 'BTREE', 'HASH',))) {
-                $index_string .= " USING {$index['type']} ";
-            }
-            $index_string .= " on `{$index['table']}` ";
-            $fields = array();
-            foreach ($index['fields'] as $f)
-            {
-                $len = intval($f['length']) ? "({$f['length']})" : '';
-                $fields[] = "{$f['name']}" . $len;
-            }
-            $index_string .= "(" . implode(',', $fields) . ")";
-        }
-        return $index_string;
-    }
-    /**
-     * create query for drop index
-     * @param $index
-     * @return string
-     */
-    protected function dropIndex($index)
-    {
-        return "DROP INDEX `{$index['name']}` ON `{$index['table']}`";
-    }
-    /**
-     * get constraint for column
-     * @param $table
-     * @param $colName
-     * @return array|bool
-     */
     protected function getConstraintForColumn($table, $colName)
     {
         $db = Zend_Db_Table::getDefaultAdapter();
@@ -425,41 +299,6 @@ class Core_Db_Database_Diff
             )
         );
         return $constraint;
-    }
-    /**
-     * create query for drop constraint
-     * @param $index
-     * @return string
-     */
-    protected function dropConstraint($index)
-    {
-        if (!isset($index['constraint']['column'])
-            || !strlen($index['constraint']['column'])
-        ) {
-            return '';
-        }
-
-        $sql = "ALTER TABLE `{$index['constraint']['table']}` " .
-            "DROP FOREIGN KEY `{$index['constraint']['name']}` ";
-
-        return $sql;
-    }
-    /**
-     * create query for adding constraint
-     * @param $index
-     * @return string
-     */
-    protected function addConstraint($index)
-    {
-        if (!isset($index['constraint']['column']) || !strlen($index['constraint']['column'])) return '';
-        $sql = "ALTER TABLE `{$index['constraint']['table']}` " .
-            "ADD CONSTRAINT `{$index['constraint']['name']}` " .
-            "FOREIGN KEY (`{$index['constraint']['column']}`) " .
-            "REFERENCES `{$index['constraint']['reference']['table']}` " .
-            "(`{$index['constraint']['reference']['column']}`) " .
-            "ON UPDATE {$index['constraint']['reference']['update']} " .
-            "ON DELETE {$index['constraint']['reference']['delete']} ";
-        return $sql;
     }
 
 }
