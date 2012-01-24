010ab13fea20d72e23c90ba47cf2369f37fff400
Alexander Khaylo <alex.khaylo@gmail.com>
2012-01-24 17:59:11 +0200

    Formatting Core_Controller_Action_Crud

--- library/Core/Controller/Action/Crud.php
+++ library/Core/Controller/Action/Crud.php
@@ -51,15 +51,15 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     {
         $this->_isDashboard();
 
-        $this->_flashMessenger = $this->_helper->getHelper( 'FlashMessenger' );
-        $this->_viewRenderer = $this->_helper->getHelper( 'viewRenderer' );
-        $this->_redirector = $this->_helper->getHelper( 'redirector' );
+        $this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
+        $this->_viewRenderer = $this->_helper->getHelper('viewRenderer');
+        $this->_redirector = $this->_helper->getHelper('redirector');
 
         /** change view script path specification */
-        $this->_after( '_changeViewScriptPathSpec', array('only' => array('index', 'grid', 'create', 'edit')) );
+        $this->_after('_changeViewScriptPathSpec', array('only' => array('index', 'grid', 'create', 'edit')));
 
         /** load grid */
-        $this->_beforeGridFilter( '_loadGrid' );
+        $this->_beforeGridFilter('_loadGrid');
     }
 
     /**
@@ -108,16 +108,16 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     {
         $table = $this->_getTable();
         $form = $this->_getCreateForm()
-            ->setAction( $this->view->url() );
+            ->setAction($this->view->url());
 
         if ($this->_request->isPost() &&
-            $form->isValid( $this->_getAllParams() )
+            $form->isValid($this->_getAllParams())
         ) {
-            $table->createRow( $form->getValues() )
+            $table->createRow($form->getValues())
                 ->save();
 
-            $this->_helper->flashMessenger( 'Successfully' );
-            $this->_helper->redirector( 'index' );
+            $this->_helper->flashMessenger('Successfully');
+            $this->_helper->redirector('index');
         }
 
         $this->view->form = $form;
@@ -133,17 +133,17 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
         $model = $this->_loadModel();
 
         $form = $this->_getEditForm()
-            ->setAction( $this->view->url() )
-            ->setDefaults( $model->toArray() );
+            ->setAction($this->view->url())
+            ->setDefaults($model->toArray());
 
         if ($this->_request->isPost() &&
-            $form->isValid( $this->_getAllParams() )
+            $form->isValid($this->_getAllParams())
         ) {
-            $model->setFromArray( $form->getValues() )
+            $model->setFromArray($form->getValues())
                 ->save();
 
-            $this->_helper->flashMessenger( 'Successfully' );
-            $this->_helper->redirector( 'index' );
+            $this->_helper->flashMessenger('Successfully');
+            $this->_helper->redirector('index');
         }
 
         $this->view->form = $form;
@@ -156,13 +156,13 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     protected function _loadModel()
     {
-        if (!$id = $this->_getParam( 'id' )) {
+        if (!$id = $this->_getParam('id')) {
             $this->_forwardNotFound();
         }
 
         $table = $this->_getTable();
 
-        if (!$model = $table->getById( $id )) {
+        if (!$model = $table->getById($id)) {
             $this->_forwardNotFound();
         }
 
@@ -176,7 +176,7 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     protected function _changeViewScriptPathSpec()
     {
-        $this->_viewRenderer->setViewScriptPathSpec( 'crud/:action.:suffix' );
+        $this->_viewRenderer->setViewScriptPathSpec('crud/:action.:suffix');
     }
 
     /**
@@ -188,7 +188,7 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     {
         $model = $this->_loadModel();
         if ($model) {
-        	$this->_helper->json( $model->delete() );
+            $this->_helper->json($model->delete());
         }
 
     }
@@ -199,13 +199,13 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     public function deleteAllAction()
     {
         $res = false;
-        if ($ids = $this->_getParam( 'ids' )) {
+        if ($ids = $this->_getParam('ids')) {
             $this->_getTable()->delete(
-                $this->_getTable()->getAdapter()->quoteInto( 'id IN (?)', array_values( $ids ) )
+                $this->_getTable()->getAdapter()->quoteInto('id IN (?)', array_values($ids))
             );
             $res = true;
         }
-        $this->_helper->json( $res );
+        $this->_helper->json($res);
     }
 
     /**
@@ -246,16 +246,16 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     protected function _loadGrid()
     {
         $grid = new Core_Grid();
-        $grid->setAdapter( $this->_getSource() )
-            ->setCurrentPageNumber( $this->_getParam( 'page', 1 ) )
-            ->setItemCountPerPage( 10 );
+        $grid->setAdapter($this->_getSource())
+            ->setCurrentPageNumber($this->_getParam('page', 1))
+            ->setItemCountPerPage(10);
 
-        if ($this->_getParam( 'orderColumn' )) {
-            $grid->setOrder( $this->_getParam( 'orderColumn' ), $this->_getParam( 'orderDirection', 'asc' ) );
+        if ($this->_getParam('orderColumn')) {
+            $grid->setOrder($this->_getParam('orderColumn'), $this->_getParam('orderDirection', 'asc'));
         }
 
-        if ($this->_getParam( 'filterColumn' )) {
-            $grid->setFilter( $this->_getParam( 'filterColumn' ), $this->_getParam( 'filterValue' ) );
+        if ($this->_getParam('filterColumn')) {
+            $grid->setFilter($this->_getParam('filterColumn'), $this->_getParam('filterValue'));
         }
 
         $this->grid = $grid;
@@ -268,12 +268,15 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     public function _addAllTableColumns()
     {
-        foreach ($this->_getTable()->info( Zend_Db_Table::COLS ) as $col) {
-            $this->grid->setColumn( $col, array(
-                    'name'  => ucfirst( $col ),
+        foreach ($this->_getTable()->info(Zend_Db_Table::COLS) as $col) {
+            $this->grid->setColumn(
+                $col,
+                array(
+                    'name'  => ucfirst($col),
                     'type'  => Core_Grid::TYPE_DATA,
                     'index' => $col
-                ) );
+                )
+            );
         }
     }
 
@@ -284,10 +287,13 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     public function _addEditColumn()
     {
-        $this->grid->setColumn( 'edit', array(
+        $this->grid->setColumn(
+            'edit',
+            array(
                 'name'      => 'Edit',
                 'formatter' => array($this, 'editLinkFormatter')
-            ) );
+            )
+        );
     }
 
     /**
@@ -297,10 +303,13 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     public function _addDeleteColumn()
     {
-        $this->grid->setColumn( 'delete', array(
+        $this->grid->setColumn(
+            'delete',
+            array(
                 'name'      => 'Delete',
                 'formatter' => array($this, 'deleteLinkFormatter')
-            ) );
+            )
+        );
     }
 
     /**
@@ -310,10 +319,13 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     public function _addCheckBoxColumn()
     {
-        $this->grid->setColumn( 'check', array(
+        $this->grid->setColumn(
+            'check',
+            array(
                 'name'      => '<input type="checkbox" id="selectAllCheckbox"/>',
                 'formatter' => array($this, 'checkBoxLinkFormatter')
-            ) );
+            )
+        );
     }
 
     /**
@@ -326,12 +338,15 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     public function editLinkFormatter($value, $row)
     {
         $link = '<a href="%s" class="edit">Edit</a>';
-        $url = $this->getHelper( 'url' )->url( array(
+        $url = $this->getHelper('url')->url(
+            array(
                 'action' => 'edit',
                 'id'     => $row['id']
-            ), 'default' );
+            ),
+            'default'
+        );
 
-        return sprintf( $link, $url );
+        return sprintf($link, $url);
     }
 
     /**
@@ -344,12 +359,15 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     public function deleteLinkFormatter($value, $row)
     {
         $link = '<a href="%s" class="delete">Delete</a>';
-        $url = $this->getHelper( 'url' )->url( array(
+        $url = $this->getHelper('url')->url(
+            array(
                 'action' => 'delete',
                 'id'     => $row['id']
-            ), 'default' );
+            ),
+            'default'
+        );
 
-        return sprintf( $link, $url );
+        return sprintf($link, $url);
     }
 
     /**
@@ -371,10 +389,10 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     public function trimFormatter($value, $row)
     {
-        if (strlen( $value ) >= 200) {
-            if (false !== ($breakpoint = strpos( $value, ' ', 200 ))) {
-                if ($breakpoint < strlen( $value ) - 1) {
-                    $value = substr( $value, 0, $breakpoint ) . ' ...';
+        if (strlen($value) >= 200) {
+            if (false !== ($breakpoint = strpos($value, ' ', 200))) {
+                if ($breakpoint < strlen($value) - 1) {
+                    $value = substr($value, 0, $breakpoint) . ' ...';
                 }
             }
         }
@@ -388,7 +406,7 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     public function stripTagsFormatter($value, $row)
     {
-        return strip_tags( $value );
+        return strip_tags($value);
     }
 
     /**
@@ -399,8 +417,8 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     protected function _addCreateButton()
     {
         $link = '<a href="%s" class="button">Create</a>';
-        $url = $this->getHelper( 'url' )->url( array('action' => 'create'), 'default' );
-        $this->view->placeholder( 'grid_buttons' )->create = sprintf( $link, $url );
+        $url = $this->getHelper('url')->url(array('action' => 'create'), 'default');
+        $this->view->placeholder('grid_buttons')->create = sprintf($link, $url);
     }
 
     /**
@@ -411,10 +429,13 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
     protected function _addDeleteButton()
     {
         $link = '<a href="%s" class="button" id="delete-all-button">Delete All</a>';
-        $url = $this->getHelper( 'url' )->url( array(
+        $url = $this->getHelper('url')->url(
+            array(
                 'action' => 'delete-all'
-            ), 'default' );
-        $this->view->placeholder( 'grid_buttons' )->deleteAll = sprintf( $link, $url );
+            ),
+            'default'
+        );
+        $this->view->placeholder('grid_buttons')->deleteAll = sprintf($link, $url);
     }
 
 
@@ -453,7 +474,7 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     protected function _setDefaultScriptPath()
     {
-        $this->_viewRenderer->setViewScriptPathSpec( '/:controller/:action.:suffix' );
+        $this->_viewRenderer->setViewScriptPathSpec('/:controller/:action.:suffix');
         return $this;
     }
 
@@ -465,7 +486,7 @@ abstract class Core_Controller_Action_Crud extends Core_Controller_Action
      */
     protected function _beforeGridFilter($function)
     {
-        $this->_before( $function, array('only' => array('index', 'grid')) );
+        $this->_before($function, array('only' => array('index', 'grid')));
     }
 
 
