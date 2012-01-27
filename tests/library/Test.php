5b6df46f6cd390454be00eab9096863ed166f30d
Alexander Khaylo <alex.khaylo@gmail.com>
2012-01-27 12:45:26 +0200

    add test to testsuite

--- tests/library/Test.php
+++ tests/library/Test.php
@@ -7,6 +7,7 @@ class Library_Test extends PHPUnit_Framework_TestSuite
         require_once 'Zend/VersionTest.php';
 
         require_once 'Core/Controller/Plugin/AclTest.php';
+        require_once 'Core/Dump/ManagerTest.php';
         require_once 'Core/Form/MultipageTest.php';
         require_once 'Core/Migration/ManagerTest.php';
 
@@ -14,10 +15,10 @@ class Library_Test extends PHPUnit_Framework_TestSuite
 
         $suite->addTest(new PHPUnit_Framework_TestSuite('Zend_VersionTest'));
         $suite->addTest(new PHPUnit_Framework_TestSuite('Core_Controller_Plugin_AclTest'));
+        $suite->addTest(new PHPUnit_Framework_TestSuite('Core_Dump_ManagerTest'));
         $suite->addTest(new PHPUnit_Framework_TestSuite('Core_Form_MultipageTest'));
         $suite->addTest(new PHPUnit_Framework_TestSuite('Core_Migration_ManagerTest'));
 
         return $suite;
     }
-
 }
