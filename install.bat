15cd0eb2f3d65db65edd5c549617594f0045f17b
AntonShevchuk <AntonShevchuk@gmail.com>
2012-02-17 17:04:58 +0200

    Design under refactoring More simple install.bat

--- install.bat
+++ install.bat
@@ -1,14 +1,6 @@
 @echo off
 set installDir=%CD%
 
-mkdir %installDir%\data\cache\
-mkdir %installDir%\data\logs\
-mkdir %installDir%\data\session\
-mkdir %installDir%\data\uploads\
-
-mkdir %installDir%\public\images\captcha\
-mkdir  %installDir%\public\uploads\
-
 cd %installDir%\bin\
 
 zfc.bat up migration
