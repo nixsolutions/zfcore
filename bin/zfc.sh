a941c32498bea098d87191a84dd8f5515f9d169c
Eugene Zabolotniy <realbaziak@gmail.com>
2012-05-25 19:29:34 +0300

    Made zfc.php available from any directory

--- bin/zfc.sh
+++ bin/zfc.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-ZF_CONFIG_FILE="./../.zf.ini"
+
+APP_DIR="$( cd "$( dirname $( dirname "${BASH_SOURCE[0]}" ) )" && pwd )"
+
+ZF_CONFIG_FILE="$APP_DIR/.zf.ini"
 export ZF_CONFIG_FILE
 
-ZEND_TOOL_INCLUDE_PATH_PREPEND="./../library"
+ZEND_TOOL_INCLUDE_PATH_PREPEND="$APP_DIR/library"
 export ZEND_TOOL_INCLUDE_PATH_PREPEND
 
 zf $@
