71197007a9d3bbb6c4eeb4dca5e0c55ee4d04990
Eugene Zabolotniy <realbaziak@gmail.com>
2012-05-29 19:41:35 +0300

    Fix error with bootstrap.php not found for ZFCore ZF Tool Providers.

--- bin/zfc.sh
+++ bin/zfc.sh
@@ -10,8 +10,10 @@ if ([ -h "${FILE_PATH}" ]) then
   done
 fi
 
+FILE_DIR=$( cd "$( dirname "${FILE_PATH}" )" && pwd )
+
 # get root directory
-ROOT_DIR="$( dirname $( cd "$( dirname "${FILE_PATH}" )" && pwd ) )"
+ROOT_DIR="$( dirname "${FILE_DIR}" )"
 
 # set up ZF Tool
 ZF_CONFIG_FILE="$ROOT_DIR/.zf.ini"
@@ -19,5 +21,8 @@ export ZF_CONFIG_FILE
 
 ZEND_TOOL_INCLUDE_PATH_PREPEND="$ROOT_DIR/library"
 export ZEND_TOOL_INCLUDE_PATH_PREPEND
+
+# run from this file directory
+cd $FILE_DIR
 
 zf $@
