7c178ca948aa1a142907b04e135c8361863439e2
Eugene Zabolotniy <realbaziak@gmail.com>
2012-05-29 19:15:31 +0300

    Fix critical bug for zfc.sh. Make it really executable from any place and even via symlink.

--- bin/zfc.sh
+++ bin/zfc.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-APP_DIR="$( cd "$( dirname $( dirname "${BASH_SOURCE[0]}" ) )" && pwd )"
+FILE_PATH="${BASH_SOURCE[0]}";
 
-ZF_CONFIG_FILE="$APP_DIR/.zf.ini"
+# resolve symlink
+if ([ -h "${FILE_PATH}" ]) then
+  while([ -h "${FILE_PATH}" ]) do
+    cd $( dirname "$FILE_PATH" )
+    FILE_PATH=$( readlink "$( basename $FILE_PATH )" )
+  done
+fi
+
+# get root directory
+ROOT_DIR="$( dirname $( cd "$( dirname "${FILE_PATH}" )" && pwd ) )"
+
+# set up ZF Tool
+ZF_CONFIG_FILE="$ROOT_DIR/.zf.ini"
 export ZF_CONFIG_FILE
 
-ZEND_TOOL_INCLUDE_PATH_PREPEND="$APP_DIR/library"
+ZEND_TOOL_INCLUDE_PATH_PREPEND="$ROOT_DIR/library"
 export ZEND_TOOL_INCLUDE_PATH_PREPEND
 
 zf $@
