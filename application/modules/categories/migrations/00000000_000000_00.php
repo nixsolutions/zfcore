5b40d3e2de5715da22e6d2fbb4dfc6343b441901
Nikolay Galkin <galkin@nixsolutions.com>
2012-07-31 23:31:39 +0300

    Code Style Fixing in categories module

--- application/modules/categories/migrations/00000000_000000_00.php
+++ application/modules/categories/migrations/00000000_000000_00.php
@@ -6,8 +6,8 @@ class Categories_Migration_00000000_000000_00 extends Core_Migration_Abstract
     public function up()
     {
         // faq table
-        $this->query("
-            CREATE TABLE `categories` (
+        $this->query(
+            "CREATE TABLE `categories` (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
               `alias` varchar(255) NOT NULL,
               `title` varchar(512) NOT NULL,
@@ -20,8 +20,8 @@ class Categories_Migration_00000000_000000_00 extends Core_Migration_Abstract
               PRIMARY KEY (`id`),
               UNIQUE KEY `categories_path` (`path`(128)),
               KEY `categories_parent` (`parentId`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-        ");
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8"
+        );
     }
 
     public function down()
