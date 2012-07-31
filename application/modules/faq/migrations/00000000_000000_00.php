19c78a65c50c0e1447c4cde979a855ccf0d3f2d0
Nikolay Galkin <galkin@nixsolutions.com>
2012-07-31 23:13:37 +0300

    Code Style Fixing in faq module

--- application/modules/faq/migrations/00000000_000000_00.php
+++ application/modules/faq/migrations/00000000_000000_00.php
@@ -5,16 +5,15 @@ class Faq_Migration_00000000_000000_00 extends Core_Migration_Abstract
 
     public function up()
     {
-        $this->query("
-            CREATE TABLE `faq` (
+        $this->query(
+            "CREATE TABLE `faq` (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
               `question` text NOT NULL,
               `answer` text NOT NULL,
               `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
               `updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
               PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-            "
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8"
         );
     }
 
--- application/modules/faq/models/Question/Form/Create.php
+++ application/modules/faq/models/Question/Form/Create.php
@@ -22,22 +22,23 @@ class Faq_Model_Question_Form_Create extends Core_Form
         $question->setLabel('Question:')
             ->setRequired(true)
             ->addDecorators($this->_inputDecorators);
-        $question->setAttrib('class','span8');
+        $question->setAttrib('class', 'span8');
 
         $this->addElement($question);
 
-        $answer = new Core_Form_Element_Redactor('answer', array(
-                      'label' => 'Answer:',
-                      'cols'  => 50,
-                      'rows'  => 15,
-                      'required' => true,
-                      'filters' => array('StringTrim'),
-                      'redactor' => array(
-                          'imageUpload'  => '/faq/images/upload/', // url or false
-                          'imageGetJson' => '/faq/images/list/',
-                          'fileUpload'   => false,
-                      )
-                   ));
+        $answer = new Core_Form_Element_Redactor(
+            'answer', array(
+                  'label' => 'Answer:',
+                  'cols'  => 50,
+                  'rows'  => 15,
+                  'required' => true,
+                  'filters' => array('StringTrim'),
+                  'redactor' => array(
+                      'imageUpload'  => '/faq/images/upload/', // url or false
+                      'imageGetJson' => '/faq/images/list/',
+                      'fileUpload'   => false,
+                  ))
+        );
         $answer->addDecorators($this->_inputDecorators);
         $this->addElement($answer);
 
